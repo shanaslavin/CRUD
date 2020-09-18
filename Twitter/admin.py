1a3bd0028b584df9d645ba8cf97295b56081fdc6
shanaslavin <shanaslavin@gmail.com>
2020-09-18 17:12:50 -0400

    created admin and models for title and message body

--- Twitter/admin.py
+++ Twitter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post #add this to import the Post model
+
+admin.site.register(Post) #add this to register the Post model
--- Twitter/models.py
+++ Twitter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+  title = models.CharField(max_length = 120, help_text = "Title of message")
+  message = models.TextField(help_text = "Send a Tweet!")
+
+  def __str__(self):
+    return self.title
